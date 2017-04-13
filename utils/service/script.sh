ab43925af3b13c4103d7c16878e860c4ab7677d5
Xueyuan Michael Han <michael-hahn@users.noreply.github.com>
2017-04-13 17:28:54 -0400

    include both ps and top for analysis

--- utils/service/script.sh
+++ utils/service/script.sh
@@ -2,6 +2,7 @@
 while :
 do 
 	ps --no-header -o "%cpu %mem" -p $(pidof camflow-provenance)
+	top -bn1 -p $(pidof camflow-provenance) | grep "^ " | awk '{ printf("%-8s %-8s\n", $9, $10); }'
 	# echo $(pidof camflow-provenance)
 	sleep 1
 done
