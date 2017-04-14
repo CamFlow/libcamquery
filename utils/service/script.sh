634a1aad87e0cd78d181a4bd452d894954a6ca7c
Michael Hahn <hanx@g.harvard.edu>
2017-04-14 04:40:13 +0100

    Generate four files of data to generate 2D CPU and memory graphs

--- utils/service/script.sh
+++ utils/service/script.sh
@@ -1,9 +1,8 @@
 #!/usr/bin/bash
-echo "%CPU %MEM"
 while :
 do 
-	ps --no-header -o "%cpu %mem" -p $(pidof camflow-provenance)
 	top -bn1 -p $(pidof camflow-provenance) | grep "^ " | awk '{ printf("%-8s %-8s\n", $9, $10); }'
+	ps --no-header -o "%cpu %mem" -p $(pidof camflow-provenance)
 	# echo $(pidof camflow-provenance)
 	sleep 1
 done
