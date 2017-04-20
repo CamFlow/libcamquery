b830f89ad17e482ff2778501859cfddaadcc3ce2
Thomas Pasquier <thomas.francis.pasquier@gmail.com>
2017-04-20 19:14:11 +0100

    seem to be working, next Michael to eval

--- utils/service/query.c
+++ utils/service/query.c
@@ -24,7 +24,7 @@ static int out_edge(prov_entry_t* node, prov_entry_t* edge){
 }
 
 static int in_edge(prov_entry_t* edge, prov_entry_t* node){
-  //print("Query: --%llx-->(%llx)", prov_type(edge), prov_type(node));
+  //print("Query: --%lld", relation_identifier(edge).id);
   if( has_label(edge, secret) ){
     if( node_type(node) == ENT_INODE_SOCKET )
       return CAMFLOW_RAISE_WARNING;
